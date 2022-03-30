954431d5afa10899ed2de4d5076a892a201c6095
Michael Gillund <michaelgillund@gmail.com>
2022-03-30 00:54:00 -0500

    Initial Commit

---
+++ Shared/ToastNotificationApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToastNotificationApp.swift
+//  Shared
+//
+//  Created by Michael Gillund on 3/30/22.
+//
+
+import SwiftUI
+
+@main
+struct ToastNotificationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
